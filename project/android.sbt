61556d3e6444cb7c99c79b0b20b6ef75304c78a7
Matthew Chartier <mattc@eyewyre.com>
2015-12-13 14:40:33 -0500

    basic additions

--- project/android.sbt
+++ project/android.sbt
@@ -1,1 +1,5 @@
+resolvers += "Sonatype snapshots" at "https://oss.sonatype.org/content/repositories/snapshots/"
+
+addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.7.0-SNAPSHOT")
+
 addSbtPlugin("com.hanhuy.sbt" % "android-sdk-plugin" % "1.5.11")
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,24 +1,76 @@
 package com.eyewyre
 
-import android.app.Activity
 import android.content.Intent
 import android.content.res.Configuration
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.ActionBarActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View.OnClickListener
+import android.app.Activity
+import android.view.{Menu, MenuItem, View}
+import android.widget.ListView
 
 
-class MainActivity extends Activity {
-	override def onCreate(savedInstanceState: Bundle): Unit = {
+class MainActivity extends ActionBarActivity {
+  var drawerLayout: DrawerLayout = null
+  var drawerView: ListView = null
+  var drawerToggle: ActionBarDrawerToggle = null
+
+
+  override def onCreate(savedInstanceState: Bundle): Unit = {
+    setTheme(R.style.AppTheme)
     super.onCreate(savedInstanceState)
     setContentView(R.layout.main)
 
-    val v = List(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
-    Log.wtf("app", v.map(c => (c * 100)).mkString(","))
+    drawerLayout = findViewById(R.id.drawer_layout).asInstanceOf[DrawerLayout]
 
-		Log.wtf("app", "************ Test")
+    getSupportActionBar.setDisplayHomeAsUpEnabled(true)
+    getSupportActionBar.setHomeButtonEnabled(true)
+
+    drawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START)
+
+    drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_open, R.string.drawer_close) {
+      override def onDrawerClosed(view: View) {
+        getSupportActionBar.setTitle("Scala App Template")
+        invalidateOptionsMenu()
+      }
+
+      override def onDrawerOpened(drawer: View) {
+        getSupportActionBar.setTitle("Settings")
+        invalidateOptionsMenu()
+      }
+    }
+
+    drawerLayout.setDrawerListener(drawerToggle)
 	}
+
+  override def onCreateOptionsMenu(menu: Menu): Boolean = {
+    getMenuInflater.inflate(R.menu.menu_main, menu)
+    true
+  }
+
+  override def onOptionsItemSelected(item: MenuItem): Boolean = {
+    val id = item.getItemId
+
+    if(drawerToggle.onOptionsItemSelected(item)) true
+    else if(id == R.id.action_settings) {
+      // show settings activity
+      true
+    }
+
+    else super.onOptionsItemSelected(item)
+  }
+
+  override def onPostCreate(savedInstanceState: Bundle) {
+    super.onPostCreate(savedInstanceState)
+    drawerToggle.syncState()
+  }
+
+  override def onConfigurationChanged(newConfig: Configuration) {
+    super.onConfigurationChanged(newConfig)
+    drawerToggle.onConfigurationChanged(newConfig)
+  }
+
 }
